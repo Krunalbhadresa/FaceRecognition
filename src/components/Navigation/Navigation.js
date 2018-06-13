8e895f916e12daf9e1f6713fd446c6adcfe3f52c
Krunal Bhadresha <krunalbhadresa@gmail.com>
2018-06-13 14:43:20 -0400

    basic structure

---
+++ src/components/Navigation/Navigation.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Navigation = () => {
+	return (
+		<nav>
+		<p className='tr f4 link dim black pa3 pointer ma0'>Sign out</p>
+		</nav>
+	);
+}
+
+export default Navigation;
